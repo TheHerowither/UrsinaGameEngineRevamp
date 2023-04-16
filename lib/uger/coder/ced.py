bcd3b37d60c9f91c7caaee17c9795c355282aaa3
TheHerowither <lucland278@gmail.com>
2023-04-16 18:41:32 +0200

    Worked some more on the code editor

--- lib/uger/coder/ced.py
+++ lib/uger/coder/ced.py
@@ -1,10 +1,15 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
-class UGERCodeEditor(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.geometry("1080x720")
-        self.text_field = ScrolledText(self, width = 500, height = 100)
+from threading import Thread
+class UGERCodeEditor(object):
+    #def __init__(self):
+        #super().__init__(self)
+    def mainloop(self):
+        master = tk.Tk()
+        master.geometry("1080x720")
+        self.text_field = ScrolledText(master, width = 500, height = 100)
         self.text_field.pack()
+        master.mainloop()
     def enable(self):
-        self.mainloop()
+        th = Thread(target = self.mainloop)
+        th.start()
